8ea966ba79eddfabfcf1026b2b2d7e29e85dff55
jfisher <jfisher@connexity.com>
2017-10-30 16:49:17 +1100

    Make left rotation work

--- src/main/java/codetests/rea/toyrobot/Direction.java
+++ src/main/java/codetests/rea/toyrobot/Direction.java
@@ -1,13 +1,30 @@
 package codetests.rea.toyrobot;
 
 public enum Direction {
-  NORTH,
-  EAST,
-  SOUTH,
-  WEST;
+  NORTH(0),
+  EAST(1),
+  SOUTH(2),
+  WEST(3);
+
+  private final Integer value;
+
+  Direction(final Integer value) {
+    this.value = value;
+  }
+
+  private Direction fromValue(final Integer value) {
+    for (Direction direction : Direction.values()) {
+      if (direction.value == value) {
+        return direction;
+      }
+    }
+    throw new RuntimeException("Unknown direction value " + value);
+  }
 
   public Direction left() {
-    return this;
+    final Integer numValues = Direction.values().length;
+    final Integer leftValue = ((this.value - 1) % numValues + numValues) % numValues;
+    return fromValue(leftValue);
   }
 
   public Direction right() {
