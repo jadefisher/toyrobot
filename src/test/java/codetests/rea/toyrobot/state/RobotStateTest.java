4040e29c98b09d74a8b68296259d670c466811f9
jfisher <jfisher@connexity.com>
2017-10-30 17:31:51 +1100

    Add tests to move in all directions

--- src/test/java/codetests/rea/toyrobot/state/RobotStateTest.java
+++ src/test/java/codetests/rea/toyrobot/state/RobotStateTest.java
@@ -4,48 +4,61 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import codetests.rea.toyrobot.Direction;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit Tests for the RobotState immutable domain object
  */
 public class RobotStateTest {
+
+  private RobotState initialState;
+
+  @Before
+  public void setUp() {
+    initialState = new RobotState(Direction.NORTH, 0, 0);
+  }
+
   @Test
   public void testRobotStateLeftRotation() {
-    final RobotState robotState = new RobotState(Direction.NORTH, 0, 0);
-
-    robotState.rotateLeft();
+    initialState.rotateLeft();
 
     // Robot shouldn't change it's internals after operation applied
-    assertThat(robotState, is(new RobotState(Direction.NORTH, 0, 0)));
+    assertThat(initialState, is(new RobotState(Direction.NORTH, 0, 0)));
 
     // Translation should be applied to the returned RobotState object
-    assertThat(robotState.rotateLeft(), is(new RobotState(Direction.WEST, 0, 0)));
+    assertThat(initialState.rotateLeft(), is(new RobotState(Direction.WEST, 0, 0)));
   }
 
   @Test
   public void testRobotStateRightRotation() {
-    final RobotState robotState = new RobotState(Direction.NORTH, 0, 0);
-
-    robotState.rotateRight();
+    initialState.rotateRight();
 
     // Robot shouldn't change it's internals after operation applied
-    assertThat(robotState, is(new RobotState(Direction.NORTH, 0, 0)));
+    assertThat(initialState, is(new RobotState(Direction.NORTH, 0, 0)));
 
     // Translation should be applied to the returned RobotState object
-    assertThat(robotState.rotateRight(), is(new RobotState(Direction.EAST, 0, 0)));
+    assertThat(initialState.rotateRight(), is(new RobotState(Direction.EAST, 0, 0)));
   }
 
   @Test
   public void testRobotStateMove() {
-    final RobotState robotState = new RobotState(Direction.NORTH, 0, 0);
-
-    robotState.move();
+    initialState.move();
 
     // Robot shouldn't change it's internals after operation applied
-    assertThat(robotState, is(new RobotState(Direction.NORTH, 0, 0)));
+    assertThat(initialState, is(new RobotState(Direction.NORTH, 0, 0)));
 
-    // Translation should be applied to the returned RobotState object
-    assertThat(robotState.move(), is(new RobotState(Direction.NORTH, 0, 1)));
+    // Move in NORTH direction
+    assertThat(initialState.move(), is(new RobotState(Direction.NORTH, 0, 1)));
+
+    // Move in EAST direction
+    assertThat(initialState.rotateRight().move(), is(new RobotState(Direction.EAST, 1, 0)));
+
+    // Move in SOUTH direction
+    assertThat(initialState.rotateRight().rotateRight().move(),
+        is(new RobotState(Direction.SOUTH, 0, -1)));
+
+    // Move in WEST direction
+    assertThat(initialState.rotateLeft().move(), is(new RobotState(Direction.WEST, -1, 0)));
   }
 }
